909d08343da8559695727042fdf2a28706e3b9c3
chao.luo3 <chao.luo3@zhenai.com>
2022-05-31 20:11:38 +0800

    feat: 完善rollup配置打包

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,39 +7,12 @@ module.exports = {
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended',
-    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
-    'prettier/prettier': [
-      'error',
-      { singleQuote: true, semi: false, endOfLine: 'auto' },
-    ],
-    indent: ['error', 2],
-    '@typescript-eslint/ban-types': [
-      'error',
-      {
-        extendDefaults: true,
-        types: {
-          Function: false,
-        },
-      },
-    ],
-    '@typescript-eslint/no-explicit-any': ['off'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
-    },
-  ],
 }
